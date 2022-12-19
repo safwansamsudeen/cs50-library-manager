339a60ca538dd8d69f9cd493eedcfcbc0d791deb
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2022-12-19 11:20:02 +0530

    add transactions crud

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ def members():
         name = request.form.get("name") or request.json["name"]
         joined = request.form.get("joined") or request.json["joined"]
         debt = request.form.get("debt") or request.json["debt"]
-        print(type, name, joined)
         if type == "create":
             db.execute(
                 "INSERT INTO members (name, joined, debt ) VALUES (?, ?, ?);", name, joined, debt)
@@ -60,3 +59,35 @@ def members():
         db.execute(
             "DELETE FROM members WHERE id = ?;", id)
         return redirect('/members')
+
+
+@app.route('/transactions', methods=['GET', 'POST', 'DELETE'])
+def transactions():
+    if request.method == 'POST':
+        type = request.form.get("type") or request.json["type"]
+        member_id = request.form.get(
+            "member_select") or request.json["member_select"]
+        book_id = request.form.get(
+            "book_select") or request.json["book_select"]
+        trans_date = request.form.get(
+            "trans_date") or request.json["trans_date"]
+        trans_type = request.form.get(
+            "trans_type") or request.json["trans_type"]
+        if type == "create":
+            db.execute("INSERT INTO transactions (book_id, member_id, trans_date, trans_type) VALUES (?, ?, ?, ?);",
+                       book_id, member_id, trans_date, trans_type)
+        elif type == "update":
+            id = request.form.get('id') or request.json["id"]
+            db.execute(
+                "UPDATE transactions SET book_id = ?, member_id = ?, trans_date = ?, trans_type = ? WHERE id = ?;", book_id, member_id, trans_date, trans_type, id)
+        return redirect('/transactions')
+    elif request.method == 'GET':
+        members = db.execute("SELECT * FROM members;")
+        books = db.execute("SELECT * FROM books;")
+        transactions = db.execute("SELECT * FROM transactions;")
+        return render_template('transactions.html', members=members, books=books, transactions=transactions)
+    elif request.method == 'DELETE':
+        id = request.form.get('id') or request.json["id"]
+        db.execute(
+            "DELETE FROM transactions WHERE id = ?;", id)
+        return redirect('/transactions')
